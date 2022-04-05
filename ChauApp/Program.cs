15a492bffe8a625a28c6687b7faec9309dfae9f4
Nguyen Quoc Trong nghia <nghia.nguyen170192@gmail.com>
2022-04-05 10:40:56 +0700

    update

---
+++ ChauApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChauApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+        }
+    }
+}
